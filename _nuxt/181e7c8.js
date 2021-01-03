c24868790e80fbfbe38968450f86cb57e40987d9
Nenad Horvat <nenad@cinober.com>
2021-01-03 15:13:32 +0100

    Updates

--- _nuxt/181e7c8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{914:function(n,e,t){"use strict";t.r(e);var l={components:{Fashion:function(){return Promise.all([t.e(0),t.e(6)]).then(t.bind(null,854))}}},o=t(35),component=Object(o.a)(l,(function(){var n=this.$createElement,e=this._self._c||n;return e("div",[e("Fashion")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
